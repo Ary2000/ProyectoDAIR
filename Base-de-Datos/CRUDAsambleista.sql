38c04c4ac222c921eacf1b0c39c37b8d2542235e
Isaac <irsacfac@gmail.com>
2022-09-07 17:22:51 -0600

    Create CRUDAsambleista.sql

---
+++ Base-de-Datos/CRUDAsambleista.sql
@@ -0,0 +1,106 @@
+USE [ProyectoDAIR];
+GO
+
+IF OBJECT_ID('[dbo].[CreateAsambleista]') IS NOT NULL
+BEGIN 
+    DROP PROC [dbo].[CreateAsambleista] 
+END 
+GO
+CREATE PROC [dbo].[CreateAsambleista] 
+    @DepartamentoId INT,
+	@SectorId INT,
+	@SedeId INT,
+	@Nombre NVARCHAR(32),
+	@Cedula NVARCHAR(256)
+AS
+BEGIN
+SET NOCOUNT ON
+	BEGIN TRY
+		SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+		BEGIN TRANSACTION nuevoAsambleista
+			INSERT INTO dbo.Asambleista(DepartamentoId,
+									SectorId,
+									SedeId,
+									Nombre,
+									Cedula)
+			SELECT @DepartamentoId,
+					@SectorId,
+					@SedeId,
+					@Nombre,
+					@Cedula;
+		COMMIT TRANSACTION nuevoAsambleista;
+		SELECT @@Identity Id;
+	END TRY
+
+	BEGIN CATCH
+		IF @@TRANCOUNT>0
+			ROLLBACK TRANSACTION nuevoAsambleista;
+		SELECT -1
+	END CATCH
+SET NOCOUNT OFF
+END
+GO
+
+IF OBJECT_ID('[dbo].[ReadAsambleista]') IS NOT NULL
+BEGIN 
+    DROP PROC [dbo].[ReadAsambleista] 
+END 
+GO
+CREATE PROC [dbo].[ReadAsambleista] 
+    @Id INT
+AS
+BEGIN
+SET NOCOUNT ON
+	BEGIN TRY
+		SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+		BEGIN TRANSACTION leerAsambleista
+			SELECT D.Nombre, Sec.Nombre, Se.Nombre, Nombre, Cedula
+			FROM dbo.Asambleista A
+			INNER JOIN dbo.Departamento D ON A.DepartamentoId = D.Id
+			INNER JOIN dbo.Sector Sec ON A.SectorId = Sec.Id
+			INNER JOIN dbo.Sede Se ON A.SedeId = Se.Id
+			WHERE [Id] = @Id
+		COMMIT TRANSACTION leerAsambleista;
+		SELECT @@Identity Id;
+	END TRY
+
+	BEGIN CATCH
+		IF @@TRANCOUNT>0
+			ROLLBACK TRANSACTION leerAsambleista;
+		SELECT -1
+	END CATCH
+SET NOCOUNT OFF
+END
+GO
+
+IF OBJECT_ID('[dbo].[UpdateAsambleista]') IS NOT NULL
+BEGIN 
+    DROP PROC [dbo].[UpdateAsambleista] 
+END 
+GO
+CREATE PROC [dbo].[UpdateAsambleista]
+	@DepartamentoId INT,
+	@SectorId INT,
+	@SedeId INT,
+	@Nombre NVARCHAR(32),
+	@Cedula NVARCHAR(256)
+AS
+BEGIN
+SET NOCOUNT ON
+	BEGIN TRY
+		UPDATE [dbo].[Biomasa]
+		SET DepartamentoId = @IdUnidad,
+			SectorId = @Nombre,
+			SedeId = @Descripcion,
+			[Precio] = @Precio,
+			[Cantidad] = @Cantidad
+		WHERE Cedula = @Cedula
+				
+	END TRY
+
+	BEGIN CATCH
+		SELECT -1
+	END CATCH
+SET NOCOUNT OFF
+END
+GO
